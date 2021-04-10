843ff5be2b2836325c79a0869e20b3d7466e1623
thingkilia2507 <vikasatibrahma@gmail.com>
2021-04-10 10:19:47 +0700

    Learning about local storage (local persistence) ILT_MD_08_H Bangkit Android Class 8 Instructor : Muh Isfhani Ghiath

---
+++ app/src/main/java/com/thing/bangkit/md8_local_persistence/BookListActivity.kt
@@ -0,0 +1,11 @@
+package com.thing.bangkit.md8_local_persistence
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_list)
+    }
+}
